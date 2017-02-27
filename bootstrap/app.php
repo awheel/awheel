79780796bc388de4230759c29d115ee15874a163
fengqi <lyf362345@gmail.com>
2017-02-27 19:05:18 +0800

    Kernel 注册时判断运行环境

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,8 @@
 // 定义系统运行环境, 默认为 prod
 !defined('__ENV__') && define('__ENV__', 'prod');
 
-// 定义系统跟目录, 如果使用 Phar 打包,  需要去掉下面的 realpath 调用
-define('__ROOT__', realpath(__DIR__.'/..'));
+// 定义系统跟目录, 如果使用 Phar 打包, 需要去掉下面的 realpath 调用
+!defined('__ROOT__') && define('__ROOT__', realpath(__DIR__.'/..'));
 
 // 使用 composer 自动加载, 不要再在任何地方进行 require, include 操作
 require  __ROOT__.'/vendor/autoload.php';
@@ -12,10 +12,7 @@ require  __ROOT__.'/vendor/autoload.php';
 // 初始化应用
 $app = new light\App(__ROOT__, __ENV__);
 
-// Http Kernel
-$app->register('HttpKernel', new light\Http\Kernel($app));
-
-// Console Kernel
-$app->register('ConsoleKernel', new light\Console\Kernel($app));
+// 注册 Kernel: Console|Http
+$app->registerKernel();
 
 return $app;
